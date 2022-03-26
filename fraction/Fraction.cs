e971427eadb10bc952aba72f8c80319d37bbaa4b
klaudia.budzynska <klaudia.budzynska@PK1-18>
2022-03-26 15:53:44 +0100

    Dodaj pliki projektów.

---
+++ fraction/Fraction.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fraction
+{
+    class Fraction : IComparable<Fraction>, IEquatable<Fraction>
+    {
+        public int meter;
+        public int denominator;
+        public Fraction(int meter, int denominator)
+        {
+            if (denominator != 0)
+            {
+                this.meter = meter;
+                this.denominator = denominator;
+            } 
+            else
+            {
+                throw new ArgumentException("incorrect denominator");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{meter}/{denominator}";
+        }
+
+        public Fraction add(Fraction fraction)
+        {
+            int meterResult = meter * fraction.denominator + fraction.meter * denominator;
+            int denominatorResult = denominator * fraction.denominator;
+            Fraction result = new Fraction(meterResult, denominatorResult);
+            return result;
+        }
+
+        public int CompareTo(Fraction fraction)
+        {
+            if ((float)meter / denominator < (float)fraction.meter / fraction.denominator)
+                return -1;
+            else if ((float)meter / denominator > (float)fraction.meter / fraction.denominator)
+                return 1;
+            else return 0;
+        }
+
+        public bool Equals(Fraction otherFraction)
+        {
+            int meter1 = meter * otherFraction.denominator;
+            int meter2 = otherFraction.meter * denominator;
+            return meter1 == meter2;
+        }
+
+        public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+        {
+            int meterResult = fraction1.meter * fraction2.denominator + fraction2.meter * fraction1.denominator;
+            int denominatorResult = fraction1.denominator * fraction2.denominator;
+
+            return new Fraction(meterResult, denominatorResult);
+        }
+
+        public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+        {
+            int meterResult = fraction1.meter * fraction2.denominator - fraction2.meter * fraction1.denominator;
+            int denominatorResult = fraction1.denominator * fraction2.denominator;
+
+            return new Fraction(meterResult, denominatorResult);
+        }
+
+        public static Fraction operator /(Fraction fraction1, Fraction fraction2)
+        {
+            if (fraction2.denominator == 0)
+            {
+                throw new ArgumentException("incorrect denominator");
+            } 
+
+            int meterResult = fraction1.meter * fraction2.denominator;
+            int denominatorResult = fraction1.denominator * fraction2.meter;
+
+            return new Fraction(meterResult, denominatorResult);
+        }
+
+    }
+}
---
+++ fraction/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Fraction fraction1 = new Fraction(1, 2);
+                Console.WriteLine(fraction1);
+                
+                Fraction fraction2 = new Fraction(2, 5);
+                Console.WriteLine(fraction2);
+
+                Console.WriteLine(fraction1.add(fraction2));
+
+                Console.WriteLine(fraction1 + fraction2);
+                Console.WriteLine(fraction1 - fraction2);
+                Console.WriteLine(fraction1 / fraction2);
+
+                List<Fraction> list = new List<Fraction>();
+                list.Add(fraction1);
+                list.Add(fraction2);
+                list.Add(new Fraction(7, 3));
+                list.Add(new Fraction(4, 5));
+                Console.WriteLine("-------");
+                list.ForEach(Console.WriteLine);
+                list.Sort();
+                Console.WriteLine("-------");
+                list.ForEach(Console.WriteLine);
+
+                Console.WriteLine(fraction1.Equals(fraction2));
+
+            } catch (ArgumentException error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
